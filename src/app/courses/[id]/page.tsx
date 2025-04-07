6b2e5d1ea86a5bfb4000e650c6f3ca978b40cf6c
emmoscript <emilmr0309@gmail.com>
2025-04-07 00:54:10 -0400

    9 commit - Solucion errores de deployment

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -1,22 +1,14 @@
-// Página del lado del servidor
-import { Metadata } from 'next';
+// Componente de página asíncrono
+import type { Metadata } from 'next';
 import CourseDetailClient from './client';
 
-// No definimos nuestra propia interfaz PageProps, usamos la de Next.js
-// Usamos generics de TypeScript para la función
-export default function Page({
-  params,
-}: {
-  params: { id: string };
-}) {
+// Declaramos la función como asincrónica
+export default async function CoursePage({ params }: { params: { id: string } }) {
   return <CourseDetailClient id={params.id} />;
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { id: string };
-}): Promise<Metadata> {
+// La metadata también asincrónica
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   return {
     title: `Curso ${params.id}`,
   };
